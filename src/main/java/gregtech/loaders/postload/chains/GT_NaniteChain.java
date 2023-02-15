e90c793271532abd8b5e9a1cab49cca29ed0f1b4
chochem <40274384+chochem@users.noreply.github.com>
2023-02-15 13:53:22 +0000

    fix

--- src/main/java/gregtech/loaders/postload/chains/GT_NaniteChain.java
+++ src/main/java/gregtech/loaders/postload/chains/GT_NaniteChain.java
@@ -169,7 +169,7 @@ public class GT_NaniteChain {
                         getModItem("GoodGenerator", "huiCircuit", 1, 4) // Paradox circuit
                 },
                 new FluidStack[] { Materials.UUMatter.getFluid(500_000), Materials.RawStarMatter.getFluid(50_000),
-                        Materials.SpatialFluid.getMolten(720), },
+                        Materials.TemporalFluid.getMolten(720), },
                 new ItemStack[] { Materials.BlackDwarfMatter.getNanite(4) },
                 null,
                 null,
